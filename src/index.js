5d330ec4993e88813b1985ab4e390aff04ea0679
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-05 09:23:58 -0700

    Make a sucessful get request w/ data.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import $ from 'jquery'
+//
+// $(document).ready(() => {
+//   // have fun!
+// })
 
-$(document).ready(() => {
-  // have fun!
-})
+$( document ).ready(function() {
+    $.ajax({
+      type: 'GET',
+      url: `https://wordwatch-api.herokuapp.com/api/v1/top_word`,
+      success: function(result) {
+        var wordData= result.word
+        console.log(wordData)
+      }
+    });
+  });
+
+  
